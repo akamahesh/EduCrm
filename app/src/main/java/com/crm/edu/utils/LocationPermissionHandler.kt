f977c3550b87173f0758ff0e2c0a27b7f389aa4e
akamahesh <mckay1718@gmail.com>
2024-10-12 22:02:59 +0530

    Bugfix: Location changed from last location to active current location

--- app/src/main/java/com/crm/edu/utils/LocationPermissionHandler.kt
+++ app/src/main/java/com/crm/edu/utils/LocationPermissionHandler.kt
@@ -2,81 +2,10 @@ package com.crm.edu.utils
 
 import android.annotation.SuppressLint
 import android.location.Location
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
+import android.util.Log
+import com.google.android.gms.location.CurrentLocationRequest
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-
-
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun LocationPermissionHandler(
-    onLocationAvailable: (Location?) -> Unit,
-) {
-
-    val context = LocalContext.current
-    val locationPermissionState =
-        rememberPermissionState(android.Manifest.permission.ACCESS_FINE_LOCATION)
-
-    val fusedLocationClient = remember {
-        LocationServices.getFusedLocationProviderClient(context)
-    }
-
-    LaunchedEffect(locationPermissionState) {
-        if (locationPermissionState.status.isGranted) {
-            getLastKnownLocation(fusedLocationClient, onLocationAvailable)
-        }else {
-            locationPermissionState.launchPermissionRequest()
-        }
-    }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        when {
-            locationPermissionState.status.isGranted -> {
-                // Permission is granted
-                Text("Location permission granted")
-            }
-
-            locationPermissionState.status.shouldShowRationale -> {
-                Text("Location permission is needed to access your current location")
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(onClick = { locationPermissionState.launchPermissionRequest() }) {
-                    Text("Grant Permission")
-                }
-            }
-
-            !locationPermissionState.status.isGranted -> {
-                Text("Location permission required")
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(onClick = { locationPermissionState.launchPermissionRequest() }) {
-                    Text("Request Permission")
-                }
-            }
-        }
-
-    }
-
-}
+import com.google.android.gms.location.Priority
 
 @SuppressLint("MissingPermission")
 fun getLastKnownLocation(
@@ -84,6 +13,27 @@ fun getLastKnownLocation(
     onLocationAvailable: (Location?) -> Unit
 ) {
     fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+        Log.d("EduLogs", "Last Location: ${location.latitude}, ${location.longitude}")
         onLocationAvailable(location)
     }
+}
+
+@SuppressLint("MissingPermission")
+fun getCurrentLocation(
+    fusedLocationClient: FusedLocationProviderClient,
+    onLocationAvailable: (Location?) -> Unit
+) {
+    val currentLocationRequest = CurrentLocationRequest.Builder()
+        .setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY) // Or other priority
+        .setDurationMillis(5000) // Optional: Set a timeout
+        .setMaxUpdateAgeMillis(0) // Optional: Request the most recent location
+        .build()
+
+    fusedLocationClient.getCurrentLocation(currentLocationRequest, null)
+        .addOnSuccessListener { location ->
+            location?.let {
+                Log.d("EduLogs", "Current Location: ${it.latitude}, ${it.longitude}")
+                onLocationAvailable(it)
+            }
+        }
 }
